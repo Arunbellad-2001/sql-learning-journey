62622f03a3bdf194777ad986d395058eea68deaa
Arunbellad-2001 <arunbellad20@gmail.com>
2026-09-01 14:29:25 +0530

    learn where clause in sql

--- Employee_Table/emp_table.sql
+++ Employee_Table/emp_table.sql
@@ -42,3 +42,62 @@ VALUES
 
 select *
 from emp;
+
+-- 1 WAQ TO name and half term sal for all emp
+select name, sal*6 "half term sal"
+from emp;
+
+select *
+from emp;
+
+-- 2 WAQ TO name, deptno, sal with hike of rs100 in thier original salfor all empl
+select name, depno,(sal+100) as sal
+from emp;
+
+select *
+from emp;
+
+-- 3 WAQ TO name and annual sal with bouns of rs 2000
+select name, (sal*12+2000) "yearly sal with bouns 2000"
+from emp; 
+
+select *
+from emp;
+
+-- 4 WAQ TO name and desigination of all rmployees alonge with deduction of 50% in annual sal
+select name,job,((sal*12)/2) "annual sal with deduction of 50%"
+from emp;  
+
+select *
+from emp;
+
+-- 5 WAQ TO get the all details of employes even retrive half term sal sal
+select emp.*, sal*6 "half trem sal"
+from emp;
+
+select *
+from emp;
+
+-- Display name, job and sal for employees whose salary is greater than 2000. 
+select name,job,sal
+from emp
+where sal>2000;
+
+-- Display name and annaul salary 
+select name, sal*12 "annaul sal"
+from emp;
+
+-- Display name, job , salary for employees who are MANAGER.
+select name,job, sal
+from emp
+where JOB='MANAGER';
+
+-- Display NAME and SAL for employees whose salary is between 1000 and 3000.
+select name,sal
+from emp
+where sal>=1000 and sal<=3000; 
+
+
+
+
+ 
